2e913d1d82a579c61c55c0fa9acd8c347504c641
krakolabs <dev@krako.xyz>
2025-08-14 18:41:15 +0900

    Update hostinfo_linux.cpp

--- hostinfo_linux.cpp
+++ hostinfo_linux.cpp
@@ -1,23 +1,4 @@
-// This file is part of BOINC.
-// http://boinc.berkeley.edu
-// Copyright (C) 2018 University of California
-//
-// BOINC is free software; you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License
-// as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
-//
-// BOINC is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-// Functions for getting the OS name and version of a Linux system.
-// This is included in the Windows build because the Win client
-// needs to get info on WSL distros.
 
 #if   defined(_WIN32) && !defined(__STDWX_H__)
 #include "boinc_win.h"
